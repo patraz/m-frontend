83c18e44565acaab5636ecbc66d5c776b92aa4e5
Patryk <patrykprause@gmail.com>
2023-06-10 12:09:23 +0200

    modified:   vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.PUBLIC_URL || "/",
-  transpileDependencies: ["vuetify"],
+  pages: {
+    // This is our main page which refers to the main Javascript file
+    index: "src/main.js", 
+    // Next, we list all other pages in the application, 
+    // and let all of them refer to the main Javascript file as well.
+    other_page: "src/main.js",
+  },
 };
