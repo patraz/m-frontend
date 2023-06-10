c1cd48646527db68882268ba49c5f9180e7ccbb8
Patryk <patrykprause@gmail.com>
2023-06-10 11:40:55 +0200

    modified:   src/components/Nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -64,9 +64,9 @@
                 <ul
                     class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
-                        <a href="#" @click="random()"
+                        <button @click="random()"
                             class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-emerald-500 md:p-0 dark:text-white md:dark:text-emerald-500"
-                            aria-current="page">Losuj słowo</a>
+                            aria-current="page">Losuj słowo</button>
                     </li>
                     <li>
                         <RouterLink :to="'/about'"
