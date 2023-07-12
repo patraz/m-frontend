07f3df7d6f31fa281025c0407cb5e0766bc7d14c
Patryk <patrykprause@gmail.com>
2023-07-12 13:21:44 +0200

    changed link on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
         <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
             <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
             <img src="https://i.imgur.com/Qp709cz.png" alt="sciek-logo">
-            <span class="ml-3 text-xl">Ściek.pl</span>
+            <span class="ml-3 text-xl">Sciek.lol</span>
             </a>
             <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 by
             <a href="#" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">PTRS</a>
